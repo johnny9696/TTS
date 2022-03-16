06f4012251ec13e2870993c170f65539c552525e
johnny9696 <77708129+johnny9696@users.noreply.github.com>
2022-03-16 13:48:15 +0900

    Add files via upload

--- TTS/tts/utils/text/cleaners.py
+++ TTS/tts/utils/text/cleaners.py
@@ -1,16 +1,25 @@
-"""Set of default text cleaners"""
-# TODO: pick the cleaner for languages dynamically
+ """
+Cleaners are transformations that run over the input text at both training and eval time.
+
+Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
+hyperparameter. Some cleaners are English-specific. You'll typically want to use:
+  1. "english_cleaners" for English text
+  2. "transliteration_cleaners" for non-English text that can be transliterated to ASCII using
+     the Unidecode library (https://pypi.python.org/pypi/Unidecode)
+  3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
+     the symbols in symbols.py to match your data).
+"""
 
 import re
 
-from anyascii import anyascii
+from unidecode import unidecode
+from unicodedata import normalize
 
 from TTS.tts.utils.text.chinese_mandarin.numbers import replace_numbers_to_characters_in_text
 
-from .english.abbreviations import abbreviations_en
-from .english.number_norm import normalize_numbers as en_normalize_numbers
-from .english.time_norm import expand_time_english
-from .french.abbreviations import abbreviations_fr
+from .abbreviations import abbreviations_en, abbreviations_fr
+from .number_norm import normalize_numbers
+from .time import expand_time_english
 
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r"\s+")
@@ -26,6 +35,10 @@ def expand_abbreviations(text, lang="en"):
     return text
 
 
+def expand_numbers(text):
+    return normalize_numbers(text)
+
+
 def lowercase(text):
     return text.lower()
 
@@ -35,12 +48,16 @@ def collapse_whitespace(text):
 
 
 def convert_to_ascii(text):
-    return anyascii(text)
+    return unidecode(text)
 
 
 def remove_aux_symbols(text):
     text = re.sub(r"[\<\>\(\)\[\]\"]+", "", text)
     return text
+
+
+def nfd(text):
+    return normalize('NFD', text)
 
 
 def replace_symbols(text, lang="en"):
@@ -53,6 +70,8 @@ def replace_symbols(text, lang="en"):
         text = text.replace("&", " et ")
     elif lang == "pt":
         text = text.replace("&", " e ")
+    elif lang == "kr":
+        text = text.replace("&", " 앤 ")
     return text
 
 
@@ -65,7 +84,7 @@ def basic_cleaners(text):
 
 def transliteration_cleaners(text):
     """Pipeline for non-English text that transliterates to ASCII."""
-    # text = convert_to_ascii(text)
+    text = convert_to_ascii(text)
     text = lowercase(text)
     text = collapse_whitespace(text)
     return text
@@ -89,20 +108,10 @@ def basic_turkish_cleaners(text):
 
 def english_cleaners(text):
     """Pipeline for English text, including number and abbreviation expansion."""
-    # text = convert_to_ascii(text)
+    text = convert_to_ascii(text)
     text = lowercase(text)
     text = expand_time_english(text)
-    text = en_normalize_numbers(text)
-    text = expand_abbreviations(text)
-    text = replace_symbols(text)
-    text = remove_aux_symbols(text)
-    text = collapse_whitespace(text)
-    return text
-
-
-def phoneme_cleaners(text):
-    """Pipeline for phonemes mode, including number and abbreviation expansion."""
-    text = en_normalize_numbers(text)
+    text = expand_numbers(text)
     text = expand_abbreviations(text)
     text = replace_symbols(text)
     text = remove_aux_symbols(text)
@@ -136,10 +145,21 @@ def chinese_mandarin_cleaners(text: str) -> str:
     return text
 
 
-def multilingual_cleaners(text):
-    """Pipeline for multilingual text"""
+def korean_cleaners(text):
     text = lowercase(text)
-    text = replace_symbols(text, lang=None)
+    text = replace_symbols(text, lang="kr")
+    text = remove_aux_symbols(text)
+    text = collapse_whitespace(text)
+    text = nfd(text)
+    return text
+
+
+def phoneme_cleaners(text):
+    """Pipeline for phonemes mode, including number and abbreviation expansion."""
+    text = expand_numbers(text)
+    text = convert_to_ascii(text)
+    text = expand_abbreviations(text)
+    text = replace_symbols(text)
     text = remove_aux_symbols(text)
     text = collapse_whitespace(text)
     return text
